cc9db64d06b0111aa613e1e93d8d5e0ec4016307
yosepjeon <enekelx1@naver.com>
2021-03-25 18:03:36 +0900

    프로그래머스 Level2 멀쩡한 삼각형 complete

--- src/main/kotlin/programmers/level2/멀쩡한사각형/main.kt
+++ src/main/kotlin/programmers/level2/멀쩡한사각형/main.kt
@@ -3,10 +3,33 @@ package programmers.level2.멀쩡한사각형
 class Solution {
     fun solution(w: Int, h: Int): Long {
         var answer: Long = 0
-
-
+        var w = w.toLong()
+        var h = h.toLong()
+        val gcd = getGcd(w,h)
+        answer = ((w*h) - (w+h-gcd))
 
         return answer
+    }
+
+    fun getGcd(num1: Long, num2: Long):Long {
+        var n1: Long
+        var n2: Long
+        var gcd:Long = 1
+        if(num1 >= num2) {
+            n1 = num1
+            n2 = num2
+        } else {
+            n1 = num2
+            n2 = num1
+        }
+
+        for(i in 1..n2) {
+            if(n1 % i == 0.toLong() && n2 % i == 0.toLong()) {
+                gcd = i
+            }
+        }
+
+        return gcd
     }
 }
 
